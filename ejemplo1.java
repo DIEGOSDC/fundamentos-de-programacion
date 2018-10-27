6c0a9c7815aecf0dfd6e1579afafe2d9700cf162
DIEGOSDC <44125637+DIEGOSDC@users.noreply.github.com>
2018-10-26 19:12:26 -0500

    Add files via upload

---
+++ ejemplo1.java
@@ -0,0 +1,11 @@
+public class ejemplo1{
+ public static void main(String args[]){
+ 
+ int a =5, b=2;
+ double c=3.0,z;
+ z=(c+a/b)/10*5;
+  
+ System.out.println("saldo"+'\t'+'\t'+"intereses");
+ 
+ }
+}
